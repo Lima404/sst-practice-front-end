c83225efe0a1ff95613b9b1f4fe0c3adefe07c02
Lima404 <gabriel.lima2307@uol.com.br>
2024-03-24 17:28:44 -0300

    docs: create files for routes

---
+++ src/routes/Home.tsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+import './App.css'
+import Navbar from '../components/navbar'
+
+function Home() {
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default Home
