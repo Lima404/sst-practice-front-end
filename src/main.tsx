35526e61a7f8f3c3661d20714fdaa518f04e8b90
Lima404 <gabriel.lima2307@uol.com.br>
2024-03-24 17:30:18 -0300

    feat: modified some index.css and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Home from './routes/Home.tsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/login/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element= { <Home /> }/>
+        <Route path='/Login' element={ <Login /> }/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
