1e5e4ccad8f6c66b484a98fa8ddb151b3af05867
Lima404 <gabriel.lima2307@uol.com.br>
2024-03-25 22:58:23 -0300

    feat: creating window login.tsx (#1)

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,9 +1,20 @@
 import { useState } from 'react'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import { FaArrowLeft } from "react-icons/fa6";
+import Planet from "./assets/saturn.svg"
+import Universe from "./assets/universe.png"
 import './styles.css'
 
 const Login = () => {
+
+    const theme = createTheme ({
+        palette: {
+            primary: {
+                main: '#ffffff',
+            }
+        }
+    })
         
         const [usuario , setUsuario] = useState('')
         const [senha , setSenha] = useState('')
@@ -17,29 +28,35 @@ const Login = () => {
 
         return(
             <div className='main'>
-                <div className='left--side'>
-                    <div className='data--input'>
-                        <form className='form--login'>
-                            <div className="email--input">
-                                <h3>Email</h3>
-                                <TextField id="standard-basic" label="Entre com seu email!" variant="standard" />
+                <div className="card--login">
+                    <div className='left--side'>
+                        <div className='data--input'>
+                            <form className='form--login'>
+                                <div className="email--input">
+                                    <ThemeProvider theme={theme}>
+                                        <TextField id="standard-basic" label="Entre com seu email!" variant="standard" />
+                                    </ThemeProvider>
+                                </div>
+                                <div className="pass--input">
+                                    <ThemeProvider theme={theme}>
+                                        <TextField id="standard-basic" label="Digite sua senha para ter acesso!" variant="standard" />
+                                    </ThemeProvider>
+                                </div>
+                            </form>
+                            <div className='buttons'>
+                                <a className='forget--pass' href="#">Esqueci minha senha!</a>
+                                <button className='but-enter'>Entrar</button>
                             </div>
-                            <div className="email--input">
-                                <h3>Senha</h3>
-                                <TextField id="standard-basic" label="Digite sua senha para ter acesso!" variant="standard" />
-                            </div>
-                        </form>
-                        <div className='buttons'>
-                            <a href="#">Esqueci minha senha!</a>
-                            <button>Entrar</button>
-                            <div className='Back'>
-                                <a href="/"><FaArrowLeft /> Voltar para o menu principal!</a>
-                            </div>
+                                <div className='Back'>
+                                    <a className='back-to-menu' href="/"><FaArrowLeft /> Voltar para o menu principal!</a>
+                                </div>
                         </div>
                     </div>
-                </div>
-                <div className='right--side'>
-                    <h2>ola</h2>
+                    <div className='right--side'>
+                        <img src={Planet} alt="icone planeta" />
+                        <h2 className='title--login'>Entre no universo onde cada clique abre novas possibilidades.</h2>
+                        <img src={Universe} alt="image universe" />
+                    </div>
                 </div>
             </div>
         )
