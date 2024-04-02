ca1d6695d0f5fc3819d6b7e9326bbd186eefd617
Lima404 <gabriel.lima2307@uol.com.br>
2024-04-02 13:56:29 -0300

    feat: add new component hero 1

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,12 +1,14 @@
 //import { useState } from 'react'
 import './styles.css'
 import Navbar from '../../layout/navbar'
+import Hero1 from '../../layout/Hero1'
 
 function Home() {
 
   return (
     <header>
       <Navbar />
+      <Hero1 />
     </header>
   )
 }
