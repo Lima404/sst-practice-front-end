48423f0e2edaeab53d15aa4274bf869c7a6a6039
Lima404 <gabriel.lima2307@uol.com.br>
2024-04-03 18:04:39 -0300

    feat: all correct, just lack of responsiveness

--- src/components/layout/Hero2/index.tsx
+++ src/components/layout/Hero2/index.tsx
@@ -4,37 +4,48 @@ import Aso from '../../../assets/hero2/aso.svg'
 import Atendimento from '../../../assets/hero2/atendimento.svg'
 import Laudo from '../../../assets/hero2/laudo.svg'
 import Mais from '../../../assets/hero2/mais.svg'
-import Doc from '../../../assets/hero2/doc.svg'
+import Doc from '../../../assets/hero2/doc.png'
 
 const Hero2: React.FC = () => {
     return(
-        <div className='main'>
-            <div className="options">
-                <p className='Title--serv'>Nossos Serviçoes</p>
-                <div className="Options--serv">
-                    <img src={Anamnese} alt="anamnese" />
-                    <p className='Option--serv'>Anamnese ocupacional</p>
-                </div>
-                <div className="Options--serv">
-                    <img src={Aso} alt="Aso" />
-                    <p className='Option--serv'>ASO</p>
-                </div>
-                <div className="Options--serv">
-                    <img src={Laudo} alt="Laudo" />
-                    <p className='Option--serv'>Laudo Caracterizador</p>
-                </div>
-                <div className="Options--serv">
-                    <img src={Atendimento} alt="Atendimento" />
-                    <p className='Option--serv'>Atendimento ao paciente</p>
-                </div>
-                <div className="Options--serv">
-                    <img src={Mais} alt="Mais" />
-                    <p className='Option--serv'>Mais Serviços</p>
+        <div className='block--container'>
+            <div className="left">
+                <div className="options">
+
+                    <p className='Title--serv'>Nossos Serviçoes</p>
+
+                    <div className="Options--serv">
+                        <img src={Anamnese} alt="anamnese" />
+                        <p className='Option--serv'>Anamnese ocupacional</p>
+                    </div>
+
+                    <div className="Options--serv">
+                        <img src={Aso} alt="Aso" />
+                        <p className='Option--serv'>ASO</p>
+                    </div>
+
+                    <div className="Options--serv">
+                        <img src={Laudo} alt="Laudo" />
+                        <p className='Option--serv'>Laudo Caracterizador</p>
+                    </div>
+
+                    <div className="Options--serv">
+                        <img src={Atendimento} alt="Atendimento" />
+                        <p className='Option--serv'>Atendimento ao paciente</p>
+                    </div>
+
+                    <div className="Options--serv">
+                        <img src={Mais} alt="Mais" />
+                        <p className='Option--serv'>Mais Serviços</p>
+                    </div>
+
                 </div>
             </div>
+
             <div className="right">
                 <img className='team--doc' src={Doc} alt="team doc" />
             </div>
+
         </div>
     )
 }
--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -3,6 +3,7 @@ import './styles.css'
 import Navbar from '../../layout/navbar'
 import Hero1 from '../../layout/Hero1'
 import Hero2 from '../../layout/Hero2'
+import System from '../../layout/System'
 
 function Home() {
 
@@ -11,6 +12,7 @@ function Home() {
       <Navbar />
       <Hero1 />
       <Hero2 />
+      <System />
     </header>
   )
 }
