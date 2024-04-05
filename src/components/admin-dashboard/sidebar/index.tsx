419c609c254e64641244b3e2ae2bf8b9efffc9e1
ricsjs <ricfilho0007@gmail.com>
2024-04-05 17:30:25 -0300

    feat: implementing base sidebar

--- src/components/admin-dashboard/sidebar/index.tsx
+++ src/components/admin-dashboard/sidebar/index.tsx
@@ -3,7 +3,7 @@ import LogoSidebar from '../../../assets/sidebar/sstLogoSidebar.svg'
 import { useState } from 'react'
 
 import { HiOutlineClipboardDocument } from "react-icons/hi2"
-import { IoIosArrowDown } from "react-icons/io"
+import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io"
 
 const AdminSideBar = () => {
   const [showDocuments, setShowDocuments] = useState(false);
@@ -18,17 +18,26 @@ const AdminSideBar = () => {
         <div className='sidebar-logo'>
           <img src={LogoSidebar} />
         </div>
+
         <div className='document-select'>
-          <button onClick={toggleDocuments}>
-            <HiOutlineClipboardDocument /> Documentos <IoIosArrowDown />
+          <button className='button-select' onClick={toggleDocuments}>
+            <div className='button-select-intern'>
+              <div className='left-button-position'><HiOutlineClipboardDocument /> Documentos</div>
+              <div className='right-button-position'><IoIosArrowDown /></div>
+            </div>
           </button>
           {showDocuments && (
             <div className='document-options'>
-              <button>Opção um</button>
-              <button>Opção dois</button>
+              <button className='button-select'>
+                <div className='button-select-intern-option'>
+                  <div className='left-button-position'>Unidade 1</div>
+                  <div className='right-button-position'><IoIosArrowForward /></div>
+                </div>
+              </button>
             </div>
           )}
         </div>
+        
       </div>
     </nav>
   )
