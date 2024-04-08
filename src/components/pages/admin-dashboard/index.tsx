d0e230719d01e48848bb5aa996ac7af2db09700f
ricsjs <ricfilho0007@gmail.com>
2024-04-08 12:20:41 -0300

    feat: preparing to implement hamburger menu

--- src/components/pages/admin-dashboard/index.tsx
+++ src/components/pages/admin-dashboard/index.tsx
@@ -4,10 +4,15 @@ import './index.css'
 function AdminDashboard() {
   return (
     <div className='main-admin-dashboard'>
-        <AdminSideBar />
+      <AdminSideBar />
+      
+        <div className='hamburger-menu'>
+          Hamburger Menu
+        </div>
+      
       <div className='content'>
         <div>
-          ola
+          Conteúdo
         </div>
       </div>
     </div>
