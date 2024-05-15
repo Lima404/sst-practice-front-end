91f91a649789a0384fed1f5df6782cbb890a8079
Breno Porfirio <brenoporfirio03@gmail.com>
2024-05-15 14:29:18 -0300

    refactor: styles for company visualization table

--- src/components/pages/admin-dashboard/companies/fetch-all-companies/index.tsx
+++ src/components/pages/admin-dashboard/companies/fetch-all-companies/index.tsx
@@ -2,7 +2,15 @@ import "./index.css";
 import CompaniesTable from "./table-companies";
 
 const Companies = () => {
-  return <CompaniesTable />;
-};
+  return (
+    <div className="main-companies-admin-dashboard">
+      <div className="companies-admin-dashboard-content">
+        <div className="companies-table">
+          <CompaniesTable />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Companies;
