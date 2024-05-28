44f29c64bab7d89470f0003fe51035b872b662b0
ricsjs <ricfilho0007@gmail.com>
2024-05-27 21:36:31 -0300

    feat: adding google maps in homepage

--- src/components/home/Contact/index.tsx
+++ src/components/home/Contact/index.tsx
@@ -1,7 +1,8 @@
 import "./styles.css";
 import Instagram from "../../../assets/contass/instagram.svg";
 import Face from "../../../assets/contass/icon.svg";
-import FormMen from "../FormMen";
+// import FormMen from "../FormMen";
+// import { lazy } from "react";
 
 const Contact = () => {
   return (
@@ -46,30 +47,8 @@ const Contact = () => {
         </div>
       </div>
       <div className="mensage">
-        <div className="card-mensage">
-          <div className="header-mensage">
-            <h4 className="tit-mensage">Deixe uma mensagem</h4>
-            <p className="ph-mensage">
-              E entraremos em contato para encontrar a melhor solução para você
-              e seu negócio.
-            </p>
-          </div>
-
-          <div className="form-area">
-            <FormMen title="Nome Completo" />
-            <FormMen title="E-mail" />
-            <FormMen title="Telefone" />
-
-            <div className="men-area">
-              <h5 className="tit-area">Mensagem</h5>
-              <textarea className="text-area-men"></textarea>
-            </div>
-
-            <div className="but-area">
-              <button className="but-form">Enviar</button>
-            </div>
-          </div>
-        </div>
+        <h3 className="tit-contact">Nosso endereço</h3>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3968.9677736910207!2d-35.19819604397402!3d-5.860155057930502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b2ff40118810d5%3A0x31765d80c554576!2sAv.%20Engenheiro%20Roberto%20Freire%2C%201962%20-%20Sala%2013%20-%20Capim%20Macio%2C%20Natal%20-%20RN%2C%2059082-095!5e0!3m2!1spt-BR!2sbr!4v1716856016751!5m2!1spt-BR!2sbr" width="100%" height={400} loading="lazy"></iframe>
       </div>
     </div>
   );
