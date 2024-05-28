e91ccbaf00b2ed91c6373802e6fdf5de449870b8
ricsjs <ricfilho0007@gmail.com>
2024-05-28 13:14:18 -0300

    feat: pulling number of admins, professionals and companies on the admin dashboard

--- src/components/pages/company-dashboard/index.tsx
+++ src/components/pages/company-dashboard/index.tsx
@@ -1,43 +1,44 @@
-import { useContext, useEffect, useState } from "react";
-import Card from "../../company-components/cards";
-import CompanySideBar from "../../company-components/sidebar";
+// import { useContext, useEffect, useState } from "react";
+// import Card from "../../company-components/cards";
+import CardsInfos from "../../company-components/card-infos";
 import "./index.css";
-import { AuthContext } from "../../../data/contexts/AuthContext";
-import { fetchCardsData } from "./api";
+// import { AuthContext } from "../../../data/contexts/AuthContext";
+// import { fetchCardsData } from "./api";
 
-interface CardData {
-  professional: string;
-  company: string;
-  employee: string;
-}
+// interface CardData {
+//   professional: string;
+//   company: string;
+//   employee: string;
+// }
 
 function CompanyDashboard() {
-  const [cardsData, setCardsData] = useState<CardData[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const { userTypeId } = useContext(AuthContext);
+  // const [cardsData, setCardsData] = useState<CardData[]>([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const { userTypeId } = useContext(AuthContext);
 
-  useEffect(() => {
-    const companyId = userTypeId ?? "";
-    fetchCardsData(companyId)
-      .then((response) => {
-        const data = response.cards.map((card: any) => ({
-          professional: card.professionalId, // Assuming professionalId contains the professional name
-          company: card.companyId, // Assuming companyId contains the company name
-          employee: card.employeeId, // Assuming employeeId contains the employee name
-        }));
-        setCardsData(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error("Erro ao buscar dados dos cards:", error);
-        setIsLoading(false);
-      });
-  }, []);
+  // useEffect(() => {
+  //   const companyId = userTypeId ?? "";
+  //   fetchCardsData(companyId)
+  //     .then((response) => {
+  //       const data = response.cards.map((card: any) => ({
+  //         professional: card.professionalId, // Assuming professionalId contains the professional name
+  //         company: card.companyId, // Assuming companyId contains the company name
+  //         employee: card.employeeId, // Assuming employeeId contains the employee name
+  //       }));
+  //       setCardsData(data);
+  //       setIsLoading(false);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Erro ao buscar dados dos cards:", error);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
 
   return (
     <div className="main-admin-dashboard">
       <div className="admin-dashboard-content">
-        {isLoading ? (
+        <CardsInfos />
+        {/* {isLoading ? (
           <p>Carregando...</p>
         ) : cardsData === null || cardsData.length === 0 ? (
           <h2 className="no-cards-found">Nenhum card encontrado</h2>
@@ -50,7 +51,7 @@ function CompanyDashboard() {
               employee={card.employee}
             />
           ))
-        )}
+        )} */}
       </div>
     </div>
   );
